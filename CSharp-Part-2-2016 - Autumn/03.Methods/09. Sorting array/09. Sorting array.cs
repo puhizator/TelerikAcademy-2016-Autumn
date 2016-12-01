56844fa65928557c86ec36c1024e3a6c7a7ea9ef
Asparuh Petrov <asparuh.petrov@gmail.com>
2016-12-01 21:46:02 +0200

    solved 09 sorting array and 10 n!

--- CSharp-Part-2-2016 - Autumn/03.Methods/09. Sorting array/09. Sorting array.cs
+++ CSharp-Part-2-2016 - Autumn/03.Methods/09. Sorting array/09. Sorting array.cs
@@ -10,6 +10,37 @@ class Program
         int[] array = new int[arrSize];
         array = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+        int[] arrayResult = sortArray(array);
 
+        for (int i = 0; i < arrayResult.Length; i++)
+        {            
+            if (i < arrayResult.Length)
+            {
+                Console.Write("{0} ", arrayResult[i]);
+            }
+            else
+            {
+                Console.WriteLine("{0}", arrayResult[i]);
+            }
+        }
+    }
+
+    static int[] sortArray (int[] array)
+    {
+        int temp = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        return array;
     }
 }
---
+++ CSharp-Part-2-2016 - Autumn/03.Methods/10. N Factorial/10. N Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(nFactorial(n));
+    }
+
+    static BigInteger nFactorial (int n)
+    {
+        BigInteger result = 1;
+
+        for (int i = 1; i <= n ; i++)
+        {
+            result = result * i;
+
+        }
+
+        return result;
+    }
+}
