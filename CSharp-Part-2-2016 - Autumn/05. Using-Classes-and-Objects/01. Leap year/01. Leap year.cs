04f8e0690b3dc6ce40b4c451c1c8a9131d3cf3c4
Asparuh Petrov <asparuh.petrov@gmail.com>
2016-12-05 01:07:09 +0200

    solved 01 and 02 tasks from Using Classes and Objects

---
+++ CSharp-Part-2-2016 - Autumn/05. Using-Classes-and-Objects/01. Leap year/01. Leap year.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.Leap_year
+{
+    class Program
+    {
+        static void Main()
+        {
+            int inputYear = int.Parse(Console.ReadLine());
+
+            bool result = DateTime.IsLeapYear(inputYear);
+
+            if (result == true)
+            {
+                Console.WriteLine("Leap");
+            }
+            else
+            {
+                Console.WriteLine("Common");
+            }
+        }
+    }
+}
---
+++ CSharp-Part-2-2016 - Autumn/05. Using-Classes-and-Objects/04. Triangle surface by side and altitude/04. Triangle surface by side and altitude.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.Triangle_surface
+{
+    class Program
+    {
+        static void Main()
+        {
+            double side = double.Parse(Console.ReadLine());
+            double altitude = double.Parse(Console.ReadLine());
+
+            double surface = (side * altitude) / 2;
+
+            Console.WriteLine("{0:F2}", surface);
+        }
+    }
+}
